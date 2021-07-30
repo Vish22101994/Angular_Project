48001ddb7577d54d07a33581cacb1ed710ef9182
Vish22101994 <microsofteee467@gmail.com>
2021-07-30 19:18:19 +0530

    "StrictIntitialzation property has been changed to false And View Child decorator is added to sideNav.ts"

--- DemoProject/src/app/side-menu-comp/side-menu-comp.component.ts
+++ DemoProject/src/app/side-menu-comp/side-menu-comp.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit ,ViewChild} from '@angular/core';
 import { Router } from '@angular/router';
 import { ElementRef } from '@angular/core';
+import { AfterViewInit } from '@angular/core';
+import { MatSelectionList } from '@angular/material/list';
+import { UserStateService } from '../user-state.service';
 
 
 @Component({
@@ -8,11 +11,12 @@ import { ElementRef } from '@angular/core';
   templateUrl: './side-menu-comp.component.html',
   styleUrls: ['./side-menu-comp.component.css']
 })
-export class SideMenuCompComponent implements OnInit {
+export class SideMenuCompComponent implements OnInit , AfterViewInit {
   isSelected:boolean = false;
+  @ViewChild('listSelect') listSelect : MatSelectionList;
   menuList:String[] = ["Home", "Feature-01","Feature-02", "Feature-03","Feature-04","Feature-05","Contact Us"];
   menuIconList:String[] = ["home", "extension","extension", "extension","extension","extension","mail"];
-  constructor(private  router:Router,private ref:ElementRef) { 
+  constructor(private  router:Router,private ref:ElementRef, private service:UserStateService) { 
     
   }
 
@@ -26,6 +30,16 @@ export class SideMenuCompComponent implements OnInit {
         let url:string = listItem.replace("-","_").toLowerCase();
         this.router.navigateByUrl("/"+url);
     }
+
+
+    ngAfterViewInit() {
+        this.service.user$.subscribe(res => {
+          if(res.isLoggedIn == true) {
+            this.listSelect.options.first.selected = true;
+          }
+        });
+    }
+    
 
 }
 
